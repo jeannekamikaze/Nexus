53dbc32cc6a618fdce5fe938bcba6a87e92a41e1
Jeanne-Kamikaze <jeannekamikaze@gmail.com>
2013-09-24 11:23:15 +0200

    Fixed hello example

--- Nexus/Hello.hs
+++ Nexus/Hello.hs
@@ -5,10 +5,10 @@ import Nexus.Actor
 type Reply = String
 data Request = Hello | Shutdown deriving Show
 
---hello :: ActorT IO (Actor Reply, Request) ()
+--hello :: ActorT (Actor Reply, Request) IO ()
 hello :: ActorM (Actor Reply, Request) ()
 hello = do
     (from, msg) <- recv
     case msg of
-        Shutdown -> return ()
-        Hello -> send "Hi there!" from >> hello
+        Shutdown -> from ! "Bye!"
+        Hello    -> from ! "Hi there!" >> hello
